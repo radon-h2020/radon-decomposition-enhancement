71877c1d2d8b8aeeb5c9c8c6651b1b2a4e328d03
Jose Antonio Perusquia <74663621+JosePerusquia@users.noreply.github.com>
2021-06-24 23:22:15 +0100

    Add files via upload

--- serviceEstimation.m
+++ serviceEstimation.m
@@ -1,14 +1,14 @@
 function serviceEstimates=serviceEstimation(traces_name)
     % Regression-based service demand estimation.
     serviceEstimates=struct();
-    [funNames,traces]=extract_inf(traces_name);
+    [funNames,traces]=extractInformation(traces_name);
     numberFuns=numel(funNames);
  
     for i=1:numberFuns
-        data=traces_by_function(funNames{i},traces);
-        times=arrival_departure_response(data);
-        queue=queue_length(times);
-        estimate=demand_est(queue,times,20,funNames(i));
+        data=tracesByFunction(funNames{i},traces);
+        times=arrivalDepartureResponse(data);
+        queue=queueLength(times);
+        estimate=demand(queue,times,20,funNames(i));
         serviceEstimates.(funNames{i})=estimate;
     end
     
